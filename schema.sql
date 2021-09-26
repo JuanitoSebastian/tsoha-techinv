112c01d3f906bba1de17efc97450c5a1366cc89b
Juan Covarrubias <me@juan.fi>
2021-09-26 13:28:33 +0300

    Configurating for Heroku

--- schema.sql
+++ schema.sql
@@ -5,6 +5,37 @@ CREATE TABLE users (
   role INTEGER
 );
 
+CREATE TABLE manufacturers (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE productions (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  starting TIMESTAMP,
+  ending TIMESTAMP
+);
+
+CREATE TABLE equipment (
+  id SERIAL PRIMARY KEY,
+  model TEXT,
+  manufacturer_id INTEGER REFERENCES manufacturers
+);
+
+CREATE TABLE inventory (
+  id SERIAL PRIMARY KEY,
+  model_id INTEGER REFERENCES equipment,
+  serialnum TEXT,
+  available BOOLEAN
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY,
+  production_id INTEGER REFERENCES productions,
+  inventory_id INTEGER REFERENCES inventory
+);
+
 CREATE TABLE userinventory (
   user_id INTEGER REFERENCES users,
   inventory_id INTEGER REFERENCES inventory
@@ -18,35 +49,4 @@ CREATE TABLE userproductions (
 CREATE TABLE userreservations (
   user_id INTEGER REFERENCES users,
   reservation_id INTEGER REFERENCES reservations
-);
-
-CREATE TABLE equipment (
-  id SERIAL PRIMARY KEY,
-  model TEXT,
-  manufacturer_id INTEGER REFERENCES manufacturers
-);
-
-CREATE TABLE manufacturers (
-  id SERIAL PRIMARY KEY,
-  name TEXT
-);
-
-CREATE TABLE inventory (
-  id SERIAL PRIMARY KEY,
-  model_id INTEGER REFERENCES equipment,
-  serialnum TEXT,
-  available BOOLEAN
-);
-
-CREATE TABLE productions (
-  id SERIAL PRIMARY KEY,
-  name TEXT,
-  starting TIMESTAMP,
-  ending TIMESTAMP
-);
-
-CREATE TABLE reservations (
-  id SERIAL PRIMARY KEY,
-  production_id INTEGER REFERENCES productions,
-  inventory_id INTEGER REFERENCES inventory
 );
